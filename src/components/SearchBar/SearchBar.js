7f8c2fe0457406b724a3543c71d1bb11d90edcbc
ossan-engineer <bodega1123@gmail.com>
2018-01-12 20:09:29 +0900

    Escape HTML entity

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -29,7 +29,7 @@ class SearchBar extends Component {
           <input placeholder='Where?' />
         </div>
         <div className='SearchBar-submit'>
-          <a>Let's Go</a>
+          <a>Let&apos;s Go</a>
         </div>
       </div>
     );
