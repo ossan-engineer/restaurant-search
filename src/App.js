ab5b9c6e4fc66dd12188581e587aa35e655ccd98
ossan-engineer <bodega1123@gmail.com>
2018-01-12 20:10:13 +0900

    Modify default App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
 import './App.css';
 
 class App extends Component { // eslint-disable-line
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
